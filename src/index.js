ba120f753bd1346f40103ec4ae1ccc9877ba089c
svetlanalf <svetlanalf.by@gmail.com>
2021-08-30 23:02:12 +0300

    feat: add function with chcking brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
+function isStartBracket(char, bracketsConfig) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0] === char) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function findBracket(char, bracketsConfig) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if ((bracketsConfig[i][0] === char) || (bracketsConfig[i][1] === char)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stackBracket = [];
+  for (let i = 0; i < str.length; i++) {
+    if (isStartBracket(str[i], bracketsConfig)) {
+      
+      let findInd = findBracket(str[i], bracketsConfig);
+      if ((bracketsConfig[findInd][0] === bracketsConfig[findInd][1]) && (stackBracket.length > 0) && (stackBracket[stackBracket.length - 1] === bracketsConfig[findInd][1])) {
+        stackBracket.pop();
+      } else {
+        stackBracket.push(str[i]);
+      }      
+    } else {
+      let findInd = findBracket(str[i], bracketsConfig);
+      if (findInd >= 0) {
+        if (stackBracket.length === 0) {
+          return false;
+        } else if (stackBracket.pop() !== bracketsConfig[findInd][0]) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+
+  if (stackBracket.length > 0) {
+    return false;
+  } 
+  return true;
 }
